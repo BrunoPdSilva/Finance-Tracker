567f75dc437e129259b02f2f2ff8c317cb522cac
Bruno Peres <bruno_16515@hotmail.com>
2022-06-27 16:03:54 -0300

    Botão de deletar transações adicionado

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -11,6 +11,8 @@ const firestoreReducer = (state, action) => {
       return { success: false, isPending: false, error: action.payload, document: null };
     case "ADDED_DOCUMENT":
       return { success: true, isPending: false, error: null, document: action.payload };
+    case "DELETED_DOCUMENT":
+      return { success: true, isPending: false, error: null, document: null };
     default:
       return state;
   }
@@ -40,7 +42,16 @@ export const useFirestore = collection => {
     }
   };
 
-  const deleteDocument = async id => {};
+  const deleteDocument = async id => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const deletedDocument = await ref.doc(id).delete();
+      dispatchIfNotCancelled({ type: "DELETED_DOCUMENT" });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: "Could not delete document" });
+    }
+  };
 
   useEffect(() => {
     return () => setIsCancelled(true);
--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,12 +1,18 @@
+import { useFirestore } from "../../hooks/useFirestore";
+
 import styles from "./Home.module.css";
 
 export function TransactionList({ transactions }) {
+  const { deleteDocument, response } = useFirestore("transactions");
+  console.log(response);
+
   return (
     <ul className={styles.transactions}>
-      {transactions.map(transaction => (
-        <li key={transaction.id}>
-          <p className={styles.name}>{transaction.name}</p>
-          <p className={styles.amount}>R${transaction.amount}</p>
+      {transactions.map(({ id, name, amount }) => (
+        <li key={id}>
+          <p className={styles.name}>{name}</p>
+          <p className={styles.amount}>R${amount}</p>
+          <button onClick={() => deleteDocument(id)}>x</button>
         </li>
       ))}
     </ul>
